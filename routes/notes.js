3eb50b5dfb1e7d2935f6dd5fa5c69c020f007023
bseton11-YJKnqQ <bseton11@gmail.com>
2022-02-13 15:48:25 -0600

    updating notes.js

---
+++ routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid'); 
+
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note/ to-do
+notes.post('/', (req, res) => {
+  console.log(req.body);
+  
+  const { note_title, note_text} = req.body;
+
+  if (req.body) {
+    const newNote = {
+      note_title,
+      note_text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, '../db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding Note');
+  }
+});
+
+module.exports = notes;
