a5b258ffebd986ad0400e62849ceb74c5f94b0d5
bseton11-YJKnqQ <bseton11@gmail.com>
2022-02-13 20:23:52 -0600

    added routes to server.js

--- server.js
+++ server.js
@@ -12,3 +12,17 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Get route for home page
+app.get('/', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/index.html'))
+)
+
+// Get route for home page
+app.get('/notes', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
