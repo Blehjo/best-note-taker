b84dcf71ae49418da6c844371f3b174c2d66d733
bseton11-YJKnqQ <bseton11@gmail.com>
2022-02-14 12:15:45 -0600

    testing functionality

--- routes/notes.js
+++ routes/notes.js
@@ -1,26 +1,26 @@
 const notes = require('express').Router();
-const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid'); 
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
-  readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+notes.get('/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for a new note/ to-do
-notes.post('/', (req, res) => {
+notes.post('/api/notes', (req, res) => {
   console.log(req.body);
   
-  const { note_title, note_text} = req.body;
+  const { noteTitle, noteText } = req.body;
 
   if (req.body) {
     const newNote = {
-      note_title,
-      note_text,
+      noteTitle,
+      noteText,
       note_id: uuid(),
     };
 
-    readAndAppend(newNote, '../db/db.json');
+    readAndAppend(newNote, './db/db.json');
     res.json(`Note added successfully 🚀`);
   } else {
     res.error('Error in adding Note');
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const uuid = require('./helpers/uuid')
-let db = require('./db/db.json')
+const db = require('./db/db.json')
+const notes = require('./db/db.json')
+const { readAndAppend, readFromFile } = require('./helpers/fsUtils')
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,6 +14,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 // Get route for home page
 app.get('/', (req, res) => 
@@ -22,6 +25,30 @@ res.sendFile(path.join(__dirname, '/public/index.html'))
 app.get('/notes', (req, res) => 
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
+
+// GET db.json to append notes from database
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+})
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+  
+  const { noteTitle, noteText } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding Note');
+  }
+})
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
